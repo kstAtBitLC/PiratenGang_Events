94ea6de6695400d5183d495f89012a25d09bc069
kstAtBitLC <klaus.stolz@bitlc.de>
2020-03-09 14:20:52 +0100

    Add project files.

---
+++ PiratenGang_Events/Gang.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratenGang_Events {
+    class Gang {
+        private List<Pirat> pirats = new List<Pirat>();
+        //public event Piratenhandler piratenHandler;
+        public event EventHandler<EventArgs> piratenHandler;
+        public event EventHandler<EventArgs> neuePiratenHandler;
+
+
+        public bool Aufbruch { get; set; } = false;
+
+        public void Add(Pirat p ) {
+            pirats.Add (p);
+        }
+
+        public void AddPirats (object source, EventArgs e) {
+            List<Pirat> lp = ( ( List<Pirat> ) source );
+            pirats.AddRange (lp);
+        }
+
+        public void Konferieren () {
+            int counter = 0;
+            List<Pirat> liste = new List<Pirat> ();
+            liste.Add (new Pirat () { Name="Arif"} );
+            liste.Add (new Pirat () { Name="Sebi"} );
+
+            while ( Aufbruch == false ) {
+
+                if ( counter > 2 ) {
+                    neuePiratenHandler ( liste, EventArgs.Empty );
+                }                
+
+                Console.Write ("Aufbrechen ?");
+                Aufbruch = Convert.ToBoolean (Console.ReadLine());
+
+                if ( Aufbruch == true ) {
+                    piratenHandler (this, EventArgs.Empty);
+                }
+
+                counter++;
+            }
+        }
+
+        public void Feiern (object source, EventArgs e) {
+            List<Pirat> lp = ( ( Gang ) source ).pirats;
+
+            foreach ( Pirat p in lp ) {
+                Console.WriteLine ("Trink Kumpel: {0}", p.Name);
+            }
+        }
+
+     }
+}
---
+++ PiratenGang_Events/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratenGang_Events {
+
+    //delegate void PiratenHandler ( EventArgs ea );
+
+    class Program {
+        static void Main ( string [] args ) {
+            Gang g = new Gang ();
+            g.Add (new Pirat () { Name="Mahmoud"} );
+            g.Add (new Pirat () { Name="Jarah"} );
+            g.Add (new Pirat () { Name="Khalil"} );
+
+            g.piratenHandler += g.Feiern;
+            g.neuePiratenHandler += g.AddPirats;
+
+            g.Konferieren ();
+
+            Console.Read ();
+        }
+    }
+}
